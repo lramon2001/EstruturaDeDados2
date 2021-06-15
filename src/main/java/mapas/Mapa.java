e2b3c00be93ef912dbc23ea8e394e269cb770f0b
Lucas Ramon <78308822+lramon2001@users.noreply.github.com>
2021-06-15 13:50:01 -0300

    adicionando metodo recuperar

--- src/main/java/mapas/Mapa.java
+++ src/main/java/mapas/Mapa.java
@@ -3,48 +3,71 @@ package mapas;
 import listasligadas.ListaLigada;
 
 
-public class Mapa <K,V>{
-    private ListaLigada<ListaLigada<Associacao<K,V>>> elementos;
+public class Mapa<K, V> {
+    private ListaLigada<ListaLigada<Associacao<K, V>>> elementos;
     private int quantidadeCategorias;
 
-    public Mapa(){
-        this.elementos= new ListaLigada<ListaLigada<Associacao<K,V>>>();
-        for(int i=0;i<this.quantidadeCategorias;i++){
-            this.elementos.inserir(new ListaLigada<Associacao<K,V>>());
+    public Mapa() {
+        this.elementos = new ListaLigada<ListaLigada<Associacao<K, V>>>();
+        for (int i = 0; i < this.quantidadeCategorias; i++) {
+            this.elementos.inserir(new ListaLigada<Associacao<K, V>>());
         }
     }
-  private int gerarNumeroEspalhamento(K chave){
-        return Math.abs(chave.hashCode()%this.quantidadeCategorias);
-  }
-  public boolean contemChave(K chave){
+
+    private int gerarNumeroEspalhamento(K chave) {
+        return Math.abs(chave.hashCode() % this.quantidadeCategorias);
+    }
+
+    public boolean contemChave(K chave) {
         int numeroEspalhamento = this.gerarNumeroEspalhamento(chave);
-        ListaLigada<Associacao<K,V>> categoria = this.elementos.recuperar(numeroEspalhamento);
-        for (int i=0;i< categoria.tamanho();i++){
-            Associacao<K,V> associacao = categoria.recuperar(i);
-            if(associacao.getChave().equals(chave)){
+        ListaLigada<Associacao<K, V>> categoria = this.elementos.recuperar(numeroEspalhamento);
+        for (int i = 0; i < categoria.tamanho(); i++) {
+            Associacao<K, V> associacao = categoria.recuperar(i);
+            if (associacao.getChave().equals(chave)) {
                 return true;
             }
         }
         return false;
-  }
-  public void remover (K chave){
+    }
+
+    public void remover(K chave) {
         int numeroEspalhamento = this.gerarNumeroEspalhamento(chave);
-        ListaLigada<Associacao<K,V>> categoria = this.elementos.recuperar(numeroEspalhamento);
-        for(int i=0;i<categoria.tamanho();i++){
-            Associacao<K,V> associacao= categoria.recuperar(i);
-            if(associacao.getChave().equals(chave)){
+        ListaLigada<Associacao<K, V>> categoria = this.elementos.recuperar(numeroEspalhamento);
+        for (int i = 0; i < categoria.tamanho(); i++) {
+            Associacao<K, V> associacao = categoria.recuperar(i);
+            if (associacao.getChave().equals(chave)) {
                 categoria.remover(associacao);
                 return;
             }
         }
-        throw new IllegalArgumentException(String.format("A chave %s não existe",chave));
-  }
-  public void adicionar (K chave, V value){
-        if(this.contemChave(chave)){
+        throw new IllegalArgumentException(String.format("A chave %s não existe", chave));
+    }
+
+    public void adicionar(K chave, V value) {
+        if (this.contemChave(chave)) {
             this.remover(chave);
         }
         int numeroEspalhamento = this.gerarNumeroEspalhamento(chave);
-        ListaLigada<Associacao<K,V>> categoria = this.elementos.recuperar(numeroEspalhamento);
-        categoria.inserir(new Associacao<K,V>(chave,value));
-  }
+        ListaLigada<Associacao<K, V>> categoria = this.elementos.recuperar(numeroEspalhamento);
+        categoria.inserir(new Associacao<K, V>(chave, value));
+    }
+
+    public V recuperar(K chave) {
+        int numeroEspalhamento = this.gerarNumeroEspalhamento(chave);
+        ListaLigada<Associacao<K, V>> categoria = this.elementos.recuperar(numeroEspalhamento);
+        for (int i = 0; i < categoria.tamanho(); i++) {
+            Associacao<K, V> associacao = categoria.recuperar(i);
+            if (associacao.getChave().equals(chave)) {
+                return associacao.getValor();
+            }
+        }
+        throw new IllegalArgumentException(String.format("A chave %s não existe", chave));
+    }
+
+    @Override
+    public String toString() {
+        return "Mapa{" +
+                "elementos=" + elementos +
+                '}';
+    }
 }
