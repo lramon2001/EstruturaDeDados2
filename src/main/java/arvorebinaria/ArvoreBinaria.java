6df0b54cdb045a3b6c105de84be93b018d32afa2
Lucas Ramon <78308822+lramon2001@users.noreply.github.com>
2021-06-16 16:56:36 -0300

    Adicionando metodo inserir recursivo

--- src/main/java/arvorebinaria/ArvoreBinaria.java
+++ src/main/java/arvorebinaria/ArvoreBinaria.java
@@ -1,4 +1,33 @@
 package arvorebinaria;
 
-public class ArvoreBinaria {
+
+
+
+public class ArvoreBinaria <T>{
+    private NoArvore<T> raiz;
+
+    public ArvoreBinaria(){
+        this.raiz=null;
+    }
+    public NoArvore<T> getRaiz(){
+        return this.raiz;
+    }
+    public void inserir(NoArvore<T>noReferencia, NoArvore<T> novoNo){
+        if(novoNo.peso()>noReferencia.peso()){
+            if(noReferencia.getNoDireito()==null){
+                noReferencia.setNoDireito(novoNo);
+            }
+            else{
+                inserir(noReferencia.getNoDireito(),novoNo);
+            }
+        }
+        else{
+            if(noReferencia.getNoEsquerdo()==null){
+                noReferencia.setNoEsquerdo(novoNo);
+            }
+            else{
+                inserir(noReferencia.getNoEsquerdo(),novoNo);
+            }
+        }
+    }
 }
